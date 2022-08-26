a389e042c84af9a73b99ec1af29faed71e0dd3e6
Sergio <goich1991@gmail.com>
2022-08-26 18:04:13 +0300

    Complete 1 task

---
+++ Homework8_1/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Task54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8 */
+
+
+void NewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortRowInMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+            Console.Write(matrix[i, j] + "\t");
+        }
+    Console.WriteLine();
+    }
+
+}
+
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+Console.WriteLine("Начальная матрица: ");
+NewMatrix(matrix);
+Console.WriteLine("Конечная матрица: ");
+SortRowInMatrix(matrix);
