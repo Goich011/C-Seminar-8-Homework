f4ef6661f8f7266f8109593d74d06d3e9676fa05
Sergio <goich1991@gmail.com>
2022-08-26 18:05:21 +0300

    Complete 6 task

---
+++ Homework8_6/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 61(ДОП). Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника.
+
+int factorial(int n)
+{
+    int i, x = 1;
+
+    for (i = 1; i <= n; i++)
+        x *= i;
+        
+    return x;
+}
+
+Console.Write("Введите число элементов в основании теугольника Паскаля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i, c;
+
+for (i = 0; i < n; i++)
+{
+    for (c = 0; c <= (n - 1); c++)
+        Console.Write(" ");
+    
+    for (c = 0; c <= i; c++)
+    {
+        Console.Write(" ");
+        Console.Write(factorial(i) / (factorial(c) * factorial(i - c)));
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.ReadLine();
